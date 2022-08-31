b5703f881e4aa09cb5c43e261d63ffc100fd7ded
pavel.lopukhov <pashapepper@gmail.com>
2022-08-31 19:06:44 +0300

    CallingDoctorAtHome startService and Myself were added

--- src/main/java/pages/CallingDoctorAtHome.java
+++ src/main/java/pages/CallingDoctorAtHome.java
@@ -2,11 +2,31 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
+import io.qameta.allure.Step;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CallingDoctorAtHome {
    private WebDriver driver;
-
+   private final By Start = By.xpath("//button[@class='button font-']");//кнопка "Начать
+   private final By clickOnMyself = By.xpath("//button[@class='btn--white btn--shadow']");//кнопка "Мне"
    public CallingDoctorAtHome (WebDriver driver){
         this.driver = driver;
+    }
+
+    //Нажимаем кнопку "Начать" на странице услуги
+    @Step("Нажать Начать")
+    public CallingDoctorAtHome startService(WebDriver driver){
+        Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
+        wait.until(ExpectedConditions.elementToBeClickable(Start));
+        driver.findElement(Start).click();
+        return new CallingDoctorAtHome(driver);
+    }
+
+    //Нажимаем кнопку "Мне" на экране-квизе "Кому нужен врач?"
+    @Step("Выбрать Мне")
+    public void Myself(WebDriver driver){
+        driver.findElement(clickOnMyself).click();
     }
 }
